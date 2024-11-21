c13c381f786f0f2614149b11ef68d5e39a680c85
Ville Pekkala <ville.pekkala@hotmail.com>
2024-11-21 22:44:38 +0200

    Add initial files

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub mod vputils;
+pub mod equation_handler;
---
+++ src/vputils.rs
@@ -0,0 +1,25 @@
+﻿pub fn s_to_double(s: &str) -> f64 {
+    if s.is_empty() {
+        return 0.0;
+    }
+
+
+
+    let result : f64 = s.parse().unwrap_or(0.0);
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::vputils::s_to_double;
+
+    fn t_s_to_double() {
+        assert_eq!(s_to_double("0"), 0.0);
+        assert_eq!(s_to_double("5"), 5.0);
+        assert_eq!(s_to_double("-5"), -5.0);
+
+
+    }
+
+}
