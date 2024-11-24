06c48974ca205f32a66c8f1d42b6a909ae7d61bb
Ville Pekkala <ville.pekkala@hotmail.com>
2024-11-24 17:05:18 +0200

    Add extern methods for s_to_double
    These may be removed because they don't really
    provide any good use. The method is simple and
    most likely will be called more times than its
    runtime takes. Most likely the invokes cost more
    than using the native method. Written as a test
    how rust code could be invoked from C#

--- src/vputils.rs
+++ src/vputils.rs
@@ -1,7 +1,28 @@
 ﻿use core::str;
+use std::ffi::{c_char, CStr};
 
-pub fn s_to_double(s: &str) -> Option<f64> {
-    s_to_double_validation(s, &mut false)
+#[allow(dead_code)]
+#[repr(C)]
+struct DoubleBoolTuple {
+    value: f64,
+    is_valid: u8,
+}
+
+#[allow(dead_code)]
+#[no_mangle]
+extern fn s_to_double_extern(s: *const c_char) -> f64 {
+    let c_str = unsafe { CStr::from_ptr(s) };
+    let s_str = c_str.to_str().unwrap();
+    s_to_double_validation(s_str).0.unwrap_or(0.0)
+}
+
+#[allow(dead_code)]
+#[no_mangle]
+extern fn s_to_double_validation_extern(s: *const c_char) -> DoubleBoolTuple {
+    let c_str = unsafe { CStr::from_ptr(s) };
+    let tuple = s_to_double_validation(c_str.to_str().unwrap());
+    let valid : u8 = if tuple.1 {1} else {0};
+    DoubleBoolTuple{value: tuple.0.unwrap_or(0.0), is_valid: valid}
 }
 
 /// Parses the given string and converts it to double value. String can contain any number of
@@ -13,10 +34,25 @@ pub fn s_to_double(s: &str) -> Option<f64> {
 /// - dash \- (only before any other valid characters. Note, abc-efg0123 is converted to -123)
 ///
 /// Returns None if string is empty or parsing fails even after dropping the invalid characters
-pub fn s_to_double_validation(s: &str, mut _valid_value: &mut bool) -> Option<f64> {
-    *_valid_value = true;
+pub fn s_to_double(s: &str) -> Option<f64> {
+    s_to_double_validation(s).0
+}
+
+/// Parses the given string and converts it to double value. String can contain any number of
+/// invalid characters but the parsing will be done by dropping the invalid characters.
+/// Valid characters are:
+/// - digits \[0-9]
+/// - comma , (is converted to dot)
+/// - dot .
+/// - dash \- (only before any other valid characters. Note, abc-efg0123 is converted to -123)
+///
+/// Returns a tuple of Option<f64> and bool
+/// First item (Option<f64>): None if string is empty or parsing fails even after dropping the invalid characters
+/// Second item (bool): is set to false if any invalid character is found or if dash is at the wrong location
+pub fn s_to_double_validation(s: &str) -> (Option<f64>, bool) {
+    let mut valid_value = true;
     if s.is_empty() {
-        return None;
+        return (None, false);
     }
 
     // Allocate the string that will be converted to double with known max value
@@ -25,18 +61,17 @@ pub fn s_to_double_validation(s: &str, mut _valid_value: &mut bool) -> Option<f6
     let chars = s.char_indices();
 
     for i in chars {
-        print!("{0:?}", i.1);
         if !i.1.is_ascii_digit() && i.1 != ',' && i.1 != '.' && i.1 != '-' {
             // If there are any value that is not any of the characters declared in 'if'
             // invalidate the result (if user needs the value to be valid)
-            *_valid_value = false;
+            valid_value = false;
             continue;
         }
 
         // If the negative sign is anywhere else than the start of the value that is parsed
         // invalidate the result (if user needs the value to be valid)
         if i.1 == '-' && !num_value.is_empty() {
-            *_valid_value = false;
+            valid_value = false;
             continue;
         }
 
@@ -48,16 +83,12 @@ pub fn s_to_double_validation(s: &str, mut _valid_value: &mut bool) -> Option<f6
         }
     }
 
-    println!();
-    println!("Num value: {0}", num_value.len());
-
     match num_value.parse() {
-        Ok(result) => {
-            Some(result) },
+        Ok(result) => (Some(result), valid_value),
         Err(err) => {
             println!("Error! {err}");
-            println!("Tried to parse: {num_value}");
-            None }
+            println!("Tried to parse: \"{num_value}\"");
+            (None, false) }
     }
 }
 
@@ -82,10 +113,9 @@ mod tests {
         assert_eq!(s_to_double("你es-5"), Some(-5.0));
         assert_eq!(s_to_double("abc-efg0123"), Some(-123f64));
 
-        let mut is_valid = false;
-        assert_eq!(s_to_double_validation("-5", &mut is_valid), Some(-5.0));
+        let mut is_valid = s_to_double_validation("-5").1;
         assert_eq!(is_valid, true);
-        assert_eq!(s_to_double_validation("你es-5", &mut is_valid), Some(-5.0));
+        is_valid = s_to_double_validation("你es-5").1;
         assert_eq!(is_valid, false);
     }
 
